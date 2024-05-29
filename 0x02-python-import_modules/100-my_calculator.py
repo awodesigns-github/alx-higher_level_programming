bd2db5d2aa4b129833963af30d897eab372c41ef
awodesigns-github <awo.designs1@gmail.com>
2024-05-30 00:20:43 +0300

    my calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """ performs basic calculations """
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+
+    if len(argv) == 3:
+        operator = argv[2]
+        match operator:
+            case '+':
+                print("{} + {} = {}".format(a, b, add(a, b)))
+                exit(0)
+            case '-':
+                print("{} + {} = {}".format(a, b, sub(a, b)))
+                exit(0)
+            case '/':
+                print("{} / {} = {}".format(a, b, div(a, b)))
+                exit(0)
+            case '*':
+                print("{} * {} = {}". format(a, b, mul(a, b)))
+                exit(0)
+            case _:
+                print("Unknown operator. Available operators: +, -, * and /")
+                exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
