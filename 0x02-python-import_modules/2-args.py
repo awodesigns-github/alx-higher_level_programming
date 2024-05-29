20585dcbc6ae0ab514e51ce96645fd44c29e1acd
awodesigns-github <awo.designs1@gmail.com>
2024-05-29 18:54:03 +0300

    Dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin
+
+if __name__ == "__main__":
+    """ print all arguments and their count """
+    import sys
+
+    count = len(sys.argv) - 1
+    i = 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+        print("1: {}".format(str(sys.argv[1])))
+    else:
+        print("{} arguments.".format(count))
+        for i in range(1, count + 1):
+            print("{}: {}".format(i, sys.argv[i]))
