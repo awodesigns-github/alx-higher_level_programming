ed7c3acaa6757b37f038c8a109c4f1ffa7a12c97
awodesigns-github <awo.designs1@gmail.com>
2024-05-30 00:50:04 +0300

    my calculator -- fixed operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
         operator = argv[2]
         match operator:
             case '+':
-                print("{} + {} = {}".format(a, b, add(a, b)))
+                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
                 exit(0)
             case '-':
-                print("{} + {} = {}".format(a, b, sub(a, b)))
+                print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
                 exit(0)
             case '/':
-                print("{} / {} = {}".format(a, b, div(a, b)))
+                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
                 exit(0)
             case '*':
-                print("{} * {} = {}". format(a, b, mul(a, b)))
+                print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
                 exit(0)
             case _:
                 print("Unknown operator. Available operators: +, -, * and /")
