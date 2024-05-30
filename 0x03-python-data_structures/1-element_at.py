39b7ef4fe6c1bde92f582de628ef1170ec55c2f9
awodesigns-github <awo.designs1@gmail.com>
2024-05-30 23:10:15 +0300

    secure access and replace element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" retrieves an element at index idx """
+
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > len(my_list) - 1:
+        return None
+    else:
+        return my_list[idx]
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" replaces an element of a list at idx """
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list) - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
