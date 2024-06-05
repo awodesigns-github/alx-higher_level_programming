5c6585f8b3d1efcc357bba03c83191a3fe933bd3
awodesigns-github <awo.designs1@gmail.com>
2024-06-05 19:14:48 +0300

    vanisher and LoL -- fixed replacment removed whitespace

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,6 +6,6 @@
 def no_c(my_string):
     if my_string == '':
         return my_string
-    new_string = my_string.translate({ord('C') : None})
-    final_string = new_string.translate({ord('c') : None})
+    new_string = my_string.translate({ord('C'): None})
+    final_string = new_string.translate({ord('c'): None})
     return final_string
