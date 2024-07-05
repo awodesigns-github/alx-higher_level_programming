2a81c689571758684cc3d655db3dec527b6b9100
awodesigns-github <awo.designs1@gmail.com>
2024-07-06 01:03:07 +0300

    multiple returns - fixed return if empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if(sentence == ""):
-        return None
+    if (sentence == ""):
+        returnedTuple = len(sentence), None
+        return returnedTuple
     returnedTuple = len(sentence), sentence[0]
     return returnedTuple
