bd089f5a3fcefc917f5ceb973764b0d8375d8310
awodesigns-github <awo.designs1@gmail.com>
2024-07-06 01:18:49 +0300

    find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def isEmpty(my_list=[]):
+    if (len(my_list) == 0):
+        return None
+    return my_list
+
+
+def max_integer(my_list=[]):
+    if (isEmpty(my_list) is None):
+        return None
+    max = 0
+    for i in range(len(my_list)):
+        if (my_list[i] > max):
+            max = my_list[i]
+    return max
