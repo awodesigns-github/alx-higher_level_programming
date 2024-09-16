144c6c26a5ab3c49d44d53615465cc08f3357c5c
awodesigns-github <awo.designs1@gmail.com>
2024-09-16 21:33:43 +0300

    Search & Replace - soln using list comprehension

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,11 +3,5 @@
 
 
 def search_replace(my_list, search, replace):
-    new_list = []
-
-    for i in my_list:
-        if i == search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
+    new_list = [replace if i == search else i for i in my_list]
     return new_list
