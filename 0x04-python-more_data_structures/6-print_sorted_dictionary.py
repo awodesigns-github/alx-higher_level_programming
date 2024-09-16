43afcb894f02a8e69671833eb1080560b92f3f15
awodesigns-github <awo.designs1@gmail.com>
2024-09-17 00:56:29 +0300

    Print sorted dictionary - added values

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print("{}".format(key))
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
