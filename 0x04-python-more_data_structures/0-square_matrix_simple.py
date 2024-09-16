0d834b7ed4e14669cdd989b4877b052116a2490d
awodesigns-github <awo.designs1@gmail.com>
2024-09-16 19:27:11 +0300

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+""" Computes the square value of all integers """
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for i in matrix:
+        new_matrix.append([j**2 for j in i])
+    return new_matrix
