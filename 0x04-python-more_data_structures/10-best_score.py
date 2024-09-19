7264bdbd5ed862c37a2b016b34103f9c5456ea46
awodesigns-github <awo.designs1@gmail.com>
2024-09-19 12:43:42 +0300

    Best score - edited condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None || a_dictionary == "":
+    if not a_dictionary or a_dictionary is None:
         return None
     return max(a_dictionary, key=a_dictionary.get)
