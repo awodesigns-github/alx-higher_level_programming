0540d50e3719d161db721ee85f500b248e40e5e0
awodesigns-github <awo.designs1@gmail.com>
2024-09-19 23:17:57 +0300

    Roman to integers - without reduce

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from functools import reduce
 """ Converts roman numerals into integers """
 
 
 def roman_to_int(roman_string):
-    if roman_string is None or not roman_string:
-        return 0;
+    if not roman_string or roman_string is None:
+        return 0
     roman_dictionary = {
         "M": 1000,
         "D": 500,
@@ -15,6 +14,14 @@ def roman_to_int(roman_string):
         "V": 5,
         "I": 1
     }
-    roman_string_uppercase = roman_string.upper()
-    numerals_list = list(map(lambda x: roman_dictionary.get(x), roman_string_uppercase))
-    return int(reduce(lambda x, y: y - x if y > x else x + y, numerals_list))
+    total = 0
+    previous_number = 0
+    for i in reversed(roman_string):
+        current_number = roman_dictionary[i]
+        
+        if current_number < previous_number:
+            total -= current_number
+        else:
+            total += current_number
+        previous_number = current_number
+    return total
