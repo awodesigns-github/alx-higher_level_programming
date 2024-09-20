fbd54ba191bbf3e459296554e085b388900693ea
awodesigns-github <awo.designs1@gmail.com>
2024-09-20 13:25:51 +0300

    Safe print list - used print() to add new line instead of '\n'

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,8 +11,10 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list:
             count += 1
             print("{}".format(i), end="")
+        print()
         return count
     else:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
+        print()
         return x
