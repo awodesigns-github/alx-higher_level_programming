da7addd53538c606b76796cf66933135104bb444
awodesigns-github <awo.designs1@gmail.com>
2024-09-20 13:11:02 +0300

    Safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Prints the elements of a list """
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            my_list[i]
+    except IndexError:
+        count = 0
+        for i in my_list:
+            count += 1
+            print("{}".format(i), end="")
+        print("\n")
+        return count
+    else:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+        print("\n")
+        return x
